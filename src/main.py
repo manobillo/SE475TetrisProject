d15191675acdbf6dd2eeaa5d5b1fa806588e9cec
manobillo <mkhan238@depaul.edu>
2024-08-27 23:29:11 -0500

    started code
    stuck on python not recognizing in terminal

--- src/main.py
+++ src/main.py
@@ -1,1 +1,25 @@
- 
+#src/main.py
+
+import pygame
+from grid import Grid
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((300, 300))
+    #Create an instance of Grid
+    grid = Grid()
+
+    running = True
+    while running:
+       screen.fill((0, 0, 0))
+       grid.draw(screen)
+       pygame.display.flip()
+
+       for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+               running = False
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
